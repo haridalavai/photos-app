46bb77be1967f93ceec50d10bfada91d2b2c6ef1
haridalavai <dalavai.hari@gmail.com>
2022-11-04 23:16:05 +0530

    Added a lot

--- src/handler.ts
+++ src/handler.ts
@@ -15,7 +15,7 @@ export const uploadPhoto = async (event: APIGatewayEvent) => {
   }
 
   try {
-    const { filename, data } = extractFile(event);
+    const parts = extractFile(event);
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log('event::', event.requestContext);
@@ -24,74 +24,85 @@ export const uploadPhoto = async (event: APIGatewayEvent) => {
       throw new Error('image is required');
     }
 
-    console.log(filename, data);
+    const result = [];
+
     console.log(BUCKET);
 
-    let image = sharp(data);
-    const metadata = await image.metadata();
+    for (let i = 0; i < parts.length; i++) {
+      const { data } = parts[i];
+      const filename = new Date(Date.now()).toISOString();
+      const image = sharp(data);
+      const metadata = await image.metadata();
 
-    if (event.requestContext.authorizer && metadata.width) {
-      const userId = event?.requestContext?.authorizer.claims.sub;
+      if (event.requestContext.authorizer && metadata.width) {
+        const userId = event?.requestContext?.authorizer.claims.sub;
 
-      await Promise.all([
-        image
-          .resize(metadata.width > 400 ? 400 : metadata.width)
-          .webp()
-          .toBuffer()
-          .then((outputBuffer) => {
-            //console.log("inside resize");
+        await Promise.all([
+          image
+            .resize(metadata.width > 400 ? 400 : metadata.width)
+            .webp()
+            .toBuffer()
+            .then((outputBuffer) => {
+              //console.log("inside resize");
 
-            return s3
-              .putObject({
-                Key: `${userId}/thumbnails/${filename}.webp`,
-                Bucket: BUCKET!,
-                Body: outputBuffer,
-              })
-              .promise();
-          }),
+              return s3
+                .putObject({
+                  Key: `${userId}/thumbnails/${filename}.webp`,
+                  Bucket: BUCKET!,
+                  Body: outputBuffer,
+                })
+                .promise();
+            }),
 
-        image
-          .resize(metadata.width > 1080 ? 1080 : metadata.width)
-          .webp()
-          .toBuffer()
-          .then((outputBuffer) => {
-            //console.log("inside resize");
-            return s3
-              .putObject({
-                Key: `${userId}/webview/${filename}.webp`,
-                Bucket: BUCKET!,
-                Body: outputBuffer,
-              })
-              .promise();
-          }),
+          image
+            .resize(metadata.width > 1080 ? 1080 : metadata.width)
+            .webp()
+            .toBuffer()
+            .then((outputBuffer) => {
+              //console.log("inside resize");
+              return s3
+                .putObject({
+                  Key: `${userId}/webview/${filename}.webp`,
+                  Bucket: BUCKET!,
+                  Body: outputBuffer,
+                })
+                .promise();
+            }),
 
-        await s3
-          .putObject({
-            Key: `${userId}/original/${filename}`,
-            Bucket: BUCKET!,
-            Body: data,
-          })
-          .promise(),
-      ]);
+          await s3
+            .putObject({
+              Key: `${userId}/original/${filename}.png`,
+              Bucket: BUCKET!,
+              Body: data,
+            })
+            .promise(),
+        ]);
 
-      const photo = Photo.build({
-        userId,
-        thumbnail: `${userId}/thumbnails/${filename}.webp`,
-        webView: `${userId}/webview/${filename}.webp`,
-        original: `${userId}/original/${filename}`,
-        name: filename,
-        timestamp: new Date(Date.now()),
-      });
+        const photo = Photo.build({
+          userId,
+          thumbnail: `${userId}/thumbnails/${filename}.webp`,
+          webView: `${userId}/webview/${filename}.webp`,
+          original: `${userId}/original/${filename}.png`,
+          name: filename,
+          timestamp: new Date(Date.now()),
+        });
 
-      await photo.save();
+        await photo.save();
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          photo,
-        }),
-      };
+        result.push(photo);
+      }
     }
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+        'Access-Control-Allow-Headers': 'content-type',
+      },
+      body: JSON.stringify({
+        result,
+      }),
+    };
   } catch (err) {
     console.log(`Error::${err}`);
     return {
@@ -102,13 +113,13 @@ export const uploadPhoto = async (event: APIGatewayEvent) => {
 };
 
 export async function getPhotos() {
-  var bucketParams = {
+  const bucketParams = {
     Bucket: BUCKET!,
   };
 
   try {
     // Call S3 to obtain a list of the objects in the bucket
-    let response = await s3.listObjects(bucketParams).promise();
+    const response = await s3.listObjects(bucketParams).promise();
     console.log(response);
 
     if (response && response.Contents) {
@@ -130,19 +141,94 @@ export async function getPhotos() {
   }
 }
 
+// function to fetch thumbnails from mongo, create presigned url and return to client
+export async function getThumbnails(event: APIGatewayEvent) {
+  console.log('event::', event);
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO URI is required');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    if (event.requestContext.authorizer) {
+      //     // get user id from event
+      const page = Number(event.queryStringParameters?.page) || 1;
+      const limit = Number(event.queryStringParameters?.limit) || 10;
+      const userId = event?.requestContext?.authorizer?.claims.sub;
+
+      // if (
+      //   event.queryStringParameters &&
+      //   event.queryStringParameters.page &&
+      //   event.queryStringParameters.limit
+      // ) {
+      //   page = parseInt(event.queryStringParameters?.page);
+      //   limit = parseInt(event.queryStringParameters?.limit);
+      // }
+      const photos = await Photo.find({ userId })
+        .skip((page - 1) * limit)
+        .limit(limit);
+
+      console.log('photos::', photos);
+
+      const thumbnails = photos.map((photo) => {
+        return {
+          name: photo.name,
+          thumbnail: s3.getSignedUrl('getObject', {
+            Bucket: BUCKET!,
+            Key: photo.thumbnail,
+            Expires: 3 * 60,
+          }),
+          webView: s3.getSignedUrl('getObject', {
+            Bucket: BUCKET!,
+            Key: photo.webView,
+            Expires: 3 * 60,
+          }),
+          original: s3.getSignedUrl('getObject', {
+            Bucket: BUCKET!,
+            Key: photo.original,
+            Expires: 3 * 60,
+          }),
+        };
+      });
+
+      console.log('thumbnails::', thumbnails);
+      const count = await Photo.countDocuments({ userId });
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        body: JSON.stringify({
+          page,
+          limit,
+          totalCount: count,
+          thumbnails,
+        }),
+        isBase64Encoded: false,
+      };
+    }
+  } catch (err) {
+    console.log(`Error::${err}`);
+    return {
+      statusCode: 500,
+      body: event,
+    };
+  }
+}
+
 function extractFile(event: any) {
   console.log(event);
-  const boundary = parseMultipart.getBoundary(event.headers['Content-Type']);
+  const boundary = parseMultipart.getBoundary(event.headers['content-type']);
   console.log(`eventBody::${event.body}`);
   console.log(`boundary::${boundary}`);
   const parts = parseMultipart.Parse(
     Buffer.from(event.body, 'base64'),
     boundary
   );
-  const [{ filename, data }] = parts;
-
-  return {
-    filename,
-    data,
-  };
+  console.log('parts::', parts.length);
+  return parts;
 }
